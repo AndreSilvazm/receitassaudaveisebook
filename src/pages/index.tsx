b77df6f394bb9b2c21df7e49900328fe15f66ade
AndreSilvazm <andre.mani2015@gmail.com>
2024-04-06 03:57:47 -0300

    Adicionando novo favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -51,8 +51,9 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Cormorant+Garamond:wght@300&family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=EB+Garamond&family=Inter:wght@100..900&family=Kode+Mono:wght@400..700&family=Lilita+One&family=Noto+Sans+Lepcha&family=Oswald:wght@200&family=Playfair+Display&family=Poiret+One&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:wght@100;500&family=Rouge+Script&family=Sacramento&family=Silkscreen:wght@400;700&family=Teko:wght@300..700&display=swap" rel="stylesheet">
-
         </link>
+
+        <meta name="description" content="Bem-vindo à nossa página de receitas saudáveis e deliciosas! Aqui você encontrará uma variedade de pratos irresistíveis que não apenas satisfazem seu paladar." />
 
       </Head>
 
